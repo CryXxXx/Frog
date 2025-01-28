a7e338d7b51b3f97bcfbe40b8e9e7220e679f91a
CryXxXx <wertywerty2324@gmail.com>
2025-01-28 20:03:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -311,7 +311,7 @@ ref.addEventListener('click', () => {
 
 function place_coef() {
     
-    fetch("https://5fba5545999a9f.lhr.life/get_last_game", {
+    fetch("https://cd02eac6f0778b.lhr.life/get_last_game", {
         method: 'GET', // Указываем метод GET
         headers: {
             'Content-Type': 'application/json' // Указываем тип контента (необязательно для GET-запроса)
@@ -365,7 +365,7 @@ function place_init_user_info() {
 
 
 function info(tg_id) {
-    fetch('https://5fba5545999a9f.lhr.life/re_info', {
+    fetch('https://cd02eac6f0778b.lhr.life/re_info', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json' // Указываем тип контента
